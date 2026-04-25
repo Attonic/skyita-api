70c6715e44d688e57adbfe416bdd9579ac61dc53
Antonio Cleison <a.cleisonn@gmail.com>
2026-04-25 10:46:20 -0300

    feat: implementa response pra SkySnapshot

--- src/main/java/io/github/skyita/dto/response/SkySnapshotResponseDto.java
+++ src/main/java/io/github/skyita/dto/response/SkySnapshotResponseDto.java
@@ -1,4 +1,20 @@
 package io.github.skyita.dto.response;
 
-public record SkySnapshotResponseDto() {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record SkySnapshotResponseDto(
+        UUID id,
+        LocalDate snapshotDate,
+        LocalDateTime generatedAt,
+        String city,
+        double latitude,
+        double longitude,
+        List<SatelliteResponseDto> satellites,
+        List<PlanetResponse> planets,
+        int satelliteCount,
+        int planetCount
+) {
 }
