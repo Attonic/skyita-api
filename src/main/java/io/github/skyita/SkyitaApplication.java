453a26a06e29ea26b2822b014000bcda215903e2
Antonio Cleison <a.cleisonn@gmail.com>
2026-04-24 22:38:23 -0300

    Commit inicial

---
+++ src/main/java/io/github/skyita/SkyitaApplication.java
@@ -0,0 +1,13 @@
+package io.github.skyita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/skyita/SkyitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.skyita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
