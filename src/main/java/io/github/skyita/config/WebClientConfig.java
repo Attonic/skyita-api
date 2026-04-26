81b834f20d1e5c3de4ef0bcd63957e0b7d64bd55
Antonio Cleison <a.cleisonn@gmail.com>
2026-04-26 14:09:34 -0300

    Feat: Implementa Web Client Config

---
+++ src/main/java/io/github/skyita/config/WebClientConfig.java
@@ -0,0 +1,18 @@
+package io.github.skyita.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, String.valueOf(MediaType.APPLICATION_JSON));
+    }
+
+}
